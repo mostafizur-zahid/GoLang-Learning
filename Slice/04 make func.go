95d119fa2486256e78ea9d7f8109afe25e8cdd7f
mostafizur-zahid <mostfizurrahmanzahid8@gmail.com>
2025-07-24 13:52:58 +0600

    make file update

--- Slice/04 make func.go
+++ Slice/04 make func.go
@@ -14,4 +14,12 @@ func main() { //make is a built-in function
 	fmt.Println(s)
 	fmt.Println(len(s))
 	fmt.Println(cap(s))
+
+	ss := make([]int, 3, 5) //[0, 0, 0] len = 3, cap = 5
+	ss[0] = 10              //[10, 0, 0] len = 3, cap = 5
+	ss[2] = 30              //[10, 0, 30] len = 3, cap = 5
+
+	fmt.Println(ss)
+	fmt.Println(len(ss))
+	fmt.Println(cap(ss))
 }
