18a097189a6d9ecca5b158b7e25ad634e5d81ddd
mostafizur-zahid <mostfizurrahmanzahid8@gmail.com>
2025-07-24 14:11:16 +0600

    empty slice update

--- Slice/05emptyslice.go
+++ Slice/05emptyslice.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var s []int //empty silce or nill slice
+	var s []int //empty silce or nill slice [] len = 0, cap = 0
+	fmt.Println(s)
+
+	s = append(s, 1) //[1] len = 1, cap = 1
 	fmt.Println(s)
 }
